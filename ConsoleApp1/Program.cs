c57884f3a4cba9fad4ddffd0965b7c868019cb23
Mitchell O'Brien <mitchell.s.obrien@gmail.com>
2023-08-01 09:43:20 +1000

    added example around conditional statements.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,10 +2,35 @@
 {
     internal class Program
     {
+        // by adding public 'Public' to the class, it allows it to be accessed from outside the class.
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            SecondMethod();
+            CheckNem(3);
         }
+
+        // defining the second method
+        static void SecondMethod()
+        {
+            Console.WriteLine("This is called in the second method of main");
+        }
+
+        public static void CheckNem(int userInput)
+        {
+            if (userInput % 2 == 0)
+            {
+                Console.WriteLine("It is an even number");
+            }
+            else
+            {
+                Console.WriteLine("It is an odd number");
+            }
+        }
+
+        //Running a loop
+
+
     }
 }
 
